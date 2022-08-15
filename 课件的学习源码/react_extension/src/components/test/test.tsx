3cd41e4ecc04ffcca7eadd9c4e8d6e18122ab99e
张慧龙 <1842768374@qq.com>
2022-08-15 21:41:15 +0800

    tie-toa

---
+++ 课件的学习源码/react_extension/src/components/test/test.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const dataBase = 
+    [{ [name: string]: 'zhl', [age: number]: 12, [gender: string]: 'male' }]
+const foo: { [string]: 'string' };
+const grandPa = () => {
+
+}
