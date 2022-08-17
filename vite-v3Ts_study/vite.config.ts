92f1107a1b846380fed516d25fab9099f1661c8e
张慧龙 <1842768374@qq.com>
2022-08-18 00:34:18 +0800

    program add

--- vite-v3Ts_study/vite.config.ts
+++ vite-v3Ts_study/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+// import vueJsx from '@vue/babel-plugin-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+});
