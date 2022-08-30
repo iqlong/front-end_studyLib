fe459ce2b49ecc7c0f2b383e5925ef5226a00e2f
zhangHuilong <huilong.zhang@air-class.com>
2022-08-30 13:30:49 +0800

    stash

---
+++ vite-v3Ts_study/src/components/father.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Son from './son.vue'
+</script>
+
+<template>
+<Son>
+    <template #header>
+        <div class="header">我是张慧龙的头</div>
+    </template>
+    <template #body="scopedSlot">
+        <div class="body">
+            <ul v-for="(val, key) in scopedSlot.mes">
+                <li>{{key}} --- {{val}}</li>
+            </ul>
+        </div>
+    </template>
+</Son>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ vite-v3Ts_study/src/components/son.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useSlots } from 'vue';
+const slots = useSlots()
+console.log(slots, Object.keys(slots), Object.values(slots));
+</script>
+
+<template>
+<div class="paper">
+    <head>
+        <slot name="header"></slot>
+    </head>
+    <body>
+        <slot name="body" :mes="{name: 'zhl', age: 12}"></slot>
+    </body>
+</div>
+</template>
+
+<style scoped lang="scss">
+    
+</style>
